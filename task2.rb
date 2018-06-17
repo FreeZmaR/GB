dcaf65c0eab5db94d0795a664f0f9db7c8b2587c
FreeZmaR <medjik11@yandex.ru>
2018-06-17 21:06:14 +0300

    FreeZmaR
    task is done

--- task2.rb
+++ task2.rb
@@ -1,8 +1,10 @@
+# Task 2
+
 iter = 0
 old = 1
 sum = 0
-temp = 0
-loop do
+temp = 1
+while iter < ARGV[0] 
     if ARGV[0] < iter
         break
     end
@@ -11,3 +13,6 @@ loop do
     old = temp
     iter++
 end
+ 
+puts sum
+
